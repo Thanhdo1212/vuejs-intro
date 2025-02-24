fcb8623b118a1b85437cbfbaa751c2562a5b5004
Do Duc Thanh <thanhdo12122001@gmail.com>
2025-02-25 00:41:37 +0700

    reactive in vue

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-import { nextTick, ref } from 'vue'
-
+import { reactive, ref } from 'vue'
 const count = ref(0)
-
-const updateCount = async () => {
-  count.value++
-  await nextTick()
-  console.log(document.getElementById('count').textContent) // ❌ Không thấy thay đổi ngay lập tức!
+const state = reactive({
+  count,
+})
+const updateCount = () => {
+  state.count++
 }
+console.log(state.count)
 </script>
-
 <template>
   <p id="count">{{ count }}</p>
   <button @click="updateCount">Tăng</button>
