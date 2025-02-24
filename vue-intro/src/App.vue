4fbae3d9fee8c3f7f6dbccd063582e02c7caeac1
Do Duc Thanh <thanhdo12122001@gmail.com>
2025-02-25 00:16:33 +0700

    update DOM, nextTick

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,26 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const msg = ref('xin chao vue 3')
-const changeMsg = () => (msg.value = 'xin chao vue 4')
+import { nextTick, ref } from 'vue'
 
-// deep reactivity
+const count = ref(0)
 
-const obj = ref({
-  nested: { count: 0 },
-  arr: ['foo', 'bar'],
-})
-const mutateDeeply = () => {
-  // những thay đổi này sẽ hoạt động như mong đợi.
-  obj.value.nested.count++
-  obj.value.arr.push('baz')
+const updateCount = async () => {
+  count.value++
+  await nextTick()
+  console.log(document.getElementById('count').textContent) // ❌ Không thấy thay đổi ngay lập tức!
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <h2>{{ obj }}</h2>
-    <button @click="changeMsg">Change message</button>
-    <button style="height: 50px; width: 200px" @click="mutateDeeply">mutate Deeply</button>
-  </div>
+  <p id="count">{{ count }}</p>
+  <button @click="updateCount">Tăng</button>
 </template>
