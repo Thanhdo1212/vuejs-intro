536da7b99127a15911c1517ea1694532d76766e1
Do Duc Thanh <thanhdo12122001@gmail.com>
2025-02-25 22:34:25 +0700

    condition in vue

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,16 +1,38 @@
-<template>
-  <div :style="[baseStyles, overridingStyles]">Hello Vue</div>
-</template>
-
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { ref } from 'vue'
 
-const baseStyles = reactive({
-  color: 'red',
-  fontSize: '20px',
-})
-
-const overridingStyles = reactive({
-  fontSize: '25px',
-})
+const userStatus = ref('online')
+const changeUserStatus = () => {
+  if (userStatus.value === 'online') {
+    userStatus.value = 'offline'
+  } else if (userStatus.value === 'offline') {
+    userStatus.value = 'busy'
+  } else if (userStatus.value === 'busy') {
+    userStatus.value = 'online'
+  }
+}
 </script>
+
+<template>
+  <div>
+    <button
+      @click="changeUserStatus"
+      style="
+        padding: 20px;
+        color: aliceblue;
+        background-color: blueviolet;
+        border-radius: 10px;
+        font-size: large;
+        box-shadow: inset;
+        width: 200px;
+      "
+    >
+      Change User Status
+    </button>
+
+    <h1 v-if="userStatus === 'online'">User is online</h1>
+    <h1 v-else-if="userStatus === 'offline'">User is offline</h1>
+    <h1 v-else-if="userStatus === 'busy'">user is busy</h1>
+    <h1 v-else>User 's status is undefined'</h1>
+  </div>
+</template>
