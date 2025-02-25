64b7f41d3c82832845c7aeed0c27e8cb2cbddee7
Do Duc Thanh <thanhdo12122001@gmail.com>
2025-02-26 00:38:11 +0700

    v-for

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,38 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
-const userStatus = ref('online')
-const changeUserStatus = () => {
-  if (userStatus.value === 'online') {
-    userStatus.value = 'offline'
-  } else if (userStatus.value === 'offline') {
-    userStatus.value = 'busy'
-  } else if (userStatus.value === 'busy') {
-    userStatus.value = 'online'
-  }
-}
+const fruit = ref([
+  {
+    name: 'apple',
+    price: 12,
+    isSoldOut: '',
+  },
+  {
+    name: 'orange',
+    price: 10,
+    isSoldOut: false,
+  },
+  {
+    name: 'banana',
+    price: 8,
+    isSoldOut: false,
+  },
+])
 </script>
 
 <template>
   <div>
-    <button
-      @click="changeUserStatus"
-      style="
-        padding: 20px;
-        color: aliceblue;
-        background-color: blueviolet;
-        border-radius: 10px;
-        font-size: large;
-        box-shadow: inset;
-        width: 200px;
-      "
-    >
-      Change User Status
-    </button>
-
-    <h1 v-if="userStatus === 'online'">User is online</h1>
-    <h1 v-else-if="userStatus === 'offline'">User is offline</h1>
-    <h1 v-else-if="userStatus === 'busy'">user is busy</h1>
-    <h1 v-else>User 's status is undefined'</h1>
+    <ul>
+      <!-- v-for with v-if -->
+      <template v-for="qua in fruit" :key="qua.name">
+        <li v-if="qua.isSoldOut == false">{{ qua.name }} and {{ qua.price }}</li>
+      </template>
+    </ul>
   </div>
 </template>
