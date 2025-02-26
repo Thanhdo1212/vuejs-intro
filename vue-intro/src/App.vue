64033b6ea22641723d66f69d8ac43b52ba22baf9
Do Duc Thanh <thanhdo12122001@gmail.com>
2025-02-26 16:43:07 +0700

    watcher

--- vue-intro/src/App.vue
+++ vue-intro/src/App.vue
@@ -1,62 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
-const checkedNames = ref([])
-const gender = ref('')
-const color = ref('')
+const question = ref('')
+const answer = ref('Questions usually contain a question mark. ;-)')
+const loading = ref(false)
+
+// Theo dõi sự thay đổi của question
+watch(question, async (newQuestion) => {
+  if (newQuestion.includes('?')) {
+    loading.value = true
+    answer.value = 'Thinking...'
+    try {
+      const res = await fetch('https://yesno.wtf/api')
+      answer.value = (await res.json()).answer
+    } catch (error) {
+      answer.value = 'Error! Could not reach the API. ' + error
+    } finally {
+      loading.value = false
+    }
+  }
+})
 </script>
 
 <template>
-  <h1>ALL THE NAME : {{ checkedNames }}</h1>
-  <input
-    id="thanh"
-    type="checkbox"
-    v-model="checkedNames"
-    style="width: 50px; height: 50px"
-    value="THANH"
-  />
-  <label for="thanh">Thanh</label>
-  <input
-    id="do"
-    type="checkbox"
-    v-model="checkedNames"
-    style="width: 50px; height: 50px"
-    value="DO"
-  />
-  <label for="do">Do</label>
-  <input
-    id="duc"
-    type="checkbox"
-    v-model="checkedNames"
-    style="width: 50px; height: 50px"
-    value="DUC"
-  />
-  <label for="duc">Duc</label>
-
-  <!-- ratio check with v-model-->
-
-  <h1>Ratio box</h1>
-  <h2>Gender : {{ gender }}</h2>
-
-  <input id="male" type="radio" v-model="gender" style="width: 50px; height: 50px" value="male" />
-  <label for="male">MALE</label>
-  <input
-    id="female"
-    type="radio"
-    v-model="gender"
-    style="width: 50px; height: 50px"
-    value="female"
-  />
-  <label for="female">FEMALE</label>
-
-  <!-- selected with v-model-->
-
-  <h1>SELECT</h1>
-  <h2>Your favorite color : {{ color }}</h2>
-  <select v-model="color">
-    <option value="" disabled>Please select one</option>
-    <option value="red">RED</option>
-    <option value="blue">BLUE</option>
-    <option value="green">GREEN</option>
-  </select>
+  <p>
+    Hỏi một câu hỏi có thể trả lời bằng "có" hoặc "không":
+    <input v-model="question" :disabled="loading" />
+  </p>
+  <p>{{ answer }}</p>
 </template>
